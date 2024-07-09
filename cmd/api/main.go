552472df9bcc5b8ac891774b717e5e1e72680355
Erendile <mstfrncvzc@gmail.com>
2024-07-09 12:42:22 +0300

    gorilla mux added instead of standard mux

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"ip-rate-control/internal/database"
 	"ip-rate-control/internal/handler"
 	"ip-rate-control/internal/middleware"
@@ -16,14 +17,12 @@ func main() {
 		log.Fatalf("Error initializing database: %v", err)
 	}
 
-	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("static"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	mux.Handle("/", middleware.RateLimitMiddleware(db, http.HandlerFunc(handler.RootHandler)))
+	router := mux.NewRouter()
+	router.Use(middleware.RateLimitMiddleware(db))
+	router.HandleFunc("/", handler.RootHandler).Methods("GET")
 
 	log.Printf("Server is running on port %s", cfg.Server.Port)
-	if err := http.ListenAndServe(":"+cfg.Server.Port, mux); err != nil {
+	if err := http.ListenAndServe(":"+cfg.Server.Port, router); err != nil {
 		log.Fatalf("Could not start server: %v\n", err)
 	}
 }
